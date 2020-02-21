d1ebf9b40f3632ac360566ee42be308ec95e6976
Pavel Korzhenko <pksmall@live.ru>
2020-02-21 21:22:55 +0200

    complete HW#1

---
+++ lesson01/AsteroidGame/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsteroidGame
+{
+    public partial class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            form.Show();
+
+            Game.Initialize(form);
+            Game.Load();
+            Game.Draw();
+
+            Application.Run(form);
+        }
+
+    }
+}
