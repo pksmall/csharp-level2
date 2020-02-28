6db0fe958b327c42dd9b626334610a6b1ae419d5
Pavel Korzhenko <pksmall@live.ru>
2020-02-28 22:12:46 +0200

    a log of interfaces

--- Tests/TestConsole/Loggers/TraceLogger.cs
+++ Tests/TestConsole/Loggers/TraceLogger.cs
@@ -1,7 +1,15 @@
-﻿namespace TestConsole.Loggers
+﻿using System;
+using System.Diagnostics;
+
+namespace TestConsole.Loggers
 {
-    public class TraceLogger : DebugLogger
+    public class TraceLogger : DebugLogger, IDisposable
     {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
         public override void Log(string message)
         {
             System.Diagnostics.Trace.WriteLine($">>>> {message}");
@@ -9,7 +17,7 @@
 
         public override void Log(string message, string category)
         {
-            System.Diagnostics.Trace.WriteLine($">>>> {message} {category}");
+            Trace.Flush();
         }
 
     }
