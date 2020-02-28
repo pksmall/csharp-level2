661847aa8329a967f361eef471dd8758bd3267af
Pavel Korzhenko <pksmall@live.ru>
2020-02-28 18:27:21 +0200

    loggers

---
+++ Tests/TestConsole/Loggers/TraceLogger.cs
@@ -0,0 +1,17 @@
+﻿namespace TestConsole.Loggers
+{
+    public class TraceLogger : DebugLogger
+    {
+        public override void Log(string message)
+        {
+            System.Diagnostics.Trace.WriteLine($">>>> {message}");
+        }
+
+        public override void Log(string message, string category)
+        {
+            System.Diagnostics.Trace.WriteLine($">>>> {message} {category}");
+        }
+
+    }
+
+}
--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,17 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using TestConsole.Loggers;
 
 namespace TestConsole
 {
     static class Program
     {
-        static void Main(string[] args)   // Ctrl+K,D
+        static void Main(string[] args)
         {
+            //Logger logger = new ListLogger();
+            //Logger logger = new FileLogger("program.log");
+            //Logger logger = new VisualStudioOutputLogger();
+            Logger logger = new TraceLogger();
+            Trace.Listeners.Add(new TextWriterTraceListener("trace.log"));
 
-            Console.ReadLine();
+            logger.LogInfo("Start program");
+
+            for (var i = 0; i < 10; i++)
+            {
+                logger.LogInfo($"Do some wokr {i + 1}");
+            }
+            logger.LogWarring("The work has been stoped.");
+
+            Trace.Flush();
+/*            var log_message = ((ListLogger)logger).message;
+
+            foreach (var message in log_message)
+            {
+                Console.WriteLine($"{message}");
+            }
+*/
+            Console.ReadKey();
         }
     }
 }
