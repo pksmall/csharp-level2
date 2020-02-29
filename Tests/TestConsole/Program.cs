1e8ae9e3cf5cbab0988a95b522cbb589c7842c1c
Pavel Korzhenko <pksmall@live.ru>
2020-02-29 11:38:09 +0200

    add student and dekanat class and implement storage class

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -7,151 +7,42 @@ namespace TestConsole
 {
     static class Program
     {
+        private static List<int> GetRandomRatings(Random rnd, int countMin, int countMax)
+        {
+            var count = rnd.Next(countMin, countMax + 1);
+            var result = new List<int>(count);
+
+            for (var i = 0; i < count; i++)
+            {
+                result.Add(rnd.Next(1, 13));
+            }
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            /*            TraceLogger trace_logger = null;
+            var dekanat = new Dekanat();
 
-                        try
-                        {
-                            trace_logger = new TraceLogger();
-                            trace_logger.Log("123");
-                        }
-                        finally
-                        {
-                            trace_logger.Dispose();
-                        }
-            */
-            using (var trace_logger = new TraceLogger())
-                trace_logger.Log("123");
-
-            //Logger logger = new ListLogger();
-            //Logger logger = new FileLogger("program.log");
-            //Logger logger = new VisualStudioOutputLogger();
-            Logger logger = new TraceLogger();
-            Trace.Listeners.Add(new TextWriterTraceListener("trace.log"));
-
-            logger.LogInfo("Start program");
-
-            for (var i = 0; i < 10; i++)
+            var rnd = new Random();
+            for (var i = 0; i < 100; i++)
             {
-                logger.LogInfo($"Do some wokr {i + 1}");
+                dekanat.Add(new Student
+                { 
+                    Name = $"Student {i + 1}",
+                    Ratings = GetRandomRatings(rnd, 20, 50)
+                });
             }
-            logger.LogWarring("The work has been stoped.");
 
-            /*            var log_message = ((ListLogger)logger).message;
+            const string students_data_file = "student.csv";
+            dekanat.SaveToFile(students_data_file);
 
-                        foreach (var message in log_message)
-                        {
-                            Console.WriteLine($"{message}");
-                        }
-            */
-            var crit_logger = new ListLogger();
-            var student_logger = new Student { Name = "Ivanov" };
-            var student_clone = (Student)student_logger.Clone();
-           
-            DoSomeCriticalWork(crit_logger);
-
-            ((ILogger)student_logger).LogError("Error!");
-            //student_logger.LogError("Some error");
-
-            var random = new Random();
-            var stundents = new Student[100];
-            for (var i = 0; i < stundents.Length; i++)
-            {
-                stundents[i] = new Student { Name = $"Student {i + 1}", Height = random.Next(150, 211) };
-            }
-            Array.Sort(stundents);
-
-            Trace.Flush();
+            var dekanat2 = new Dekanat();
+            dekanat2.LoadFromFile(students_data_file);
 
 
             Console.ReadKey();
         }
-
-        public static void DoSomeCriticalWork(ILogger log)
-        {
-            for (var i = 0; i < 10; i++)
-            {
-                log.LogInfo($"Do some very important wokr {i + 1}");
-            }
-        }
     }
 
-    public class Student : ILogger, IComparable, ICloneable
-    {
-        private List<string> _message = new List<string>();
-        public double Height { get; set; } = 175;
-        public string Name { get; set; }
-        public List<int> Ratings { get; set; } = new List<int>();
-
-        public void Log(string message)
-        {
-            Ratings.Add(message.Length);
-            _message.Add(message);
-        }
-
-/*        public void LogError(string message)
-        {
-            Log("Error: " + message);
-        }
-*/
-        void ILogger.LogError(string message)
-        {
-            Log("Error: " + message);
-        }
-
-
-        public void LogInfo(string message)
-        {
-            Log("Info: " + message);
-        }
-
-        public void LogWarring(string message)
-        {
-            Log("Warring: " + message);
-        }
-
-        public int CompareTo(object obj)
-        {
-            if(obj is Student)
-            {
-                var other_Student = (Student)obj;
-                //return StringComparer.OrdinalIgnoreCase.Compare(Name, other_Student.Name);
-                if (Height > other_Student.Height)
-                {
-                    return +1;
-                } else if (Height.Equals(other_Student.Height))
-                {
-                    return 0;
-                } else
-                {
-                    return 1;
-                }
-            } else
-            {
-                if(obj is null)
-                {
-                    throw new ArgumentNullException(nameof(obj), "Try compare student to null ");
-                }
-                throw new ArgumentException(nameof(obj), "Try compare student to " + obj.GetType().Name);
-            }
-        }
-        public override string ToString() => $"{Name} = {Height}";
-
-        public object Clone()
-        {
-            /*var new_student = new Student
-            {
-                Name = Name,
-                Height = Height,
-                Ratings = new List<int>(Ratings)
-            };*/
-
-            var new_student = (Student)MemberwiseClone();
-            new_student._message = new List<string>(_message);
-            new_student.Ratings = new List<int>(Ratings);
-
-            return new_student;
-        }
-    }
 }
---
+++ Tests/TestConsole/Storage.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestConsole
+{
+    abstract class Storage<TItem>
+    {
+        protected readonly List<TItem> _items = new List<TItem>();
+        public void Add(TItem item)
+        {
+            if (_items.Contains(item)) return;
+            _items.Add(item);
+        }
+
+        public bool Remove(TItem item)
+        {
+            return _items.Remove(item);
+        }
+
+        public bool IsContains(TItem item)
+        {
+            return _items.Contains(item);
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public abstract void SaveToFile(string fileName);
+
+        public virtual void LoadFromFile(string fileName)
+        {
+            Clear();
+        }
+    }
+
+    class Dekanat : Storage<Student>
+    {
+        public override void LoadFromFile(string fileName)
+        {
+            if (!File.Exists(fileName)) throw new FileNotFoundException("File not exists!", fileName);
+
+            base.LoadFromFile(fileName);
+
+            using(var file_reader = File.OpenText(fileName))
+            {
+                while(!file_reader.EndOfStream)
+                {
+                    var str = file_reader.ReadLine();
+                    if (string.IsNullOrWhiteSpace(str)) continue;
+                    var data_elements = str.Split(',');
+                    if (data_elements.Length == 0) continue;
+
+                    var student = new Student
+                    {
+                        Name = data_elements[0],
+                    };
+                    if (data_elements.Length > 1)
+                    {
+                        var ratings = new List<int>();
+                        for (var i = 1; i < data_elements.Length; i++)
+                        {
+                            ratings.Add(int.Parse(data_elements[i]));
+                        }
+                    }
+                    Add(student);
+                }
+            }
+        }
+
+        public override void SaveToFile(string fileName)
+        {
+            using (var file_writer = File.CreateText(fileName))
+            {
+                foreach (var student in _items)
+                {
+                    file_writer.Write(student.Name);
+                    if(student.Ratings.Count > 0)
+                    {
+                        var rating_string = string.Join(",", student.Ratings);
+                        file_writer.Write($",{rating_string}");
+                    }
+
+                    file_writer.WriteLine();
+                }
+            }
+        }
+    }
+}
