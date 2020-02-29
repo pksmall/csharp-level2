61efce0c09b44c92314d551734c8c289e0c044fe
Pavel Korzhenko <pksmall@live.ru>
2020-02-29 12:46:39 +0200

    work example with delegates

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -54,8 +54,78 @@ namespace TestConsole
             var rnd_student_name = rnd.NextValue("Ivanov", "Petrov", "Sidorov");
             var random_rating = rnd.NextValue(2, 3, 4, 5);
 
+            /*  
+                StudentProcessor processor = new StudentProcessor(GetIndexedStudentName);
+                StudentProcessor processor = GetIndexedStudentName;
+
+                var index = 0;
+                foreach(var s in dekanat2)
+                {
+                    Console.WriteLine(processor(s, index++));
+                }
+
+                Console.ReadKey();
+
+                processor = GetAvgStudentRating;
+                index = 0;
+                foreach (var s in dekanat2)
+                {
+                    Console.WriteLine(processor(s, index++));
+                }
+            */
+
+            /*      
+                ProcessStudents(dekanat2, GetIndexedStudentName);
+                Console.ReadKey();
+                ProcessStudents(dekanat2, GetAvgStudentRating);
+            */
+            //ProcessStudentsStandard(dekanat2, PrintStudent);
+
+            var metrics = GetStudentsMetrics(dekanat2, std => std.Name.Length + (int)(std.AvgRating * 10));
             Console.ReadKey();
+        }
+
+        private static void PrintStudent(Student student)
+        {
+            Console.WriteLine("Student: {0}", student.Name);
+        }
+
+        public static void ProcessStudentsStandard(IEnumerable<Student> students, Action<Student> action)
+        {
+            foreach (var s in students)
+            {
+                action(s);
+            }
+        }
+
+        public static int[] GetStudentsMetrics(IEnumerable<Student> student, Func<Student, int> GetMetrics)
+        {
+            var result = new List<int>();
+            foreach(var cstd in student)
+            {
+                result.Add(GetMetrics(cstd));
+            }
+            return result.ToArray();
+        }
+
+        public static void ProcessStudents(IEnumerable<Student> students, StudentProcessor processor)
+        {
+            var index = 0;
+            foreach (var s in students)
+            {
+                Console.WriteLine(processor(s, index++));
+            }
+        }
+        private static string GetIndexedStudentName(Student student, int index)
+        {
+            return $"{student.Name}[{index}]";
+        }
+
+        public static string GetAvgStudentRating(Student student, int index)
+        {
+            return $"[{index}]:{student.Name} - {student.AvgRating}";
         }
     }
 
+    internal delegate string StudentProcessor(Student student, int index);
 }
