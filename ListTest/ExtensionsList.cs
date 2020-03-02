3c882435dbe75841ee1280869c5db7f03216cf8c
Pavel Korzhenko <pksmall@live.ru>
2020-03-02 13:17:50 +0200

    complete task02

---
+++ ListTest/ExtensionsList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace ListTest
+{
+    public static class ExtensionsList
+    {
+        public static Dictionary<T, int> UniqCount<T>(this ICollection<T> self)
+        {
+            Dictionary<T, int> uniqDictionary = new Dictionary<T, int>();
+
+            foreach (T i in self)
+            {
+                bool contains = false;
+                foreach (var p in uniqDictionary.Keys)
+                {
+                    if (p.Equals(i))
+                    {
+                        contains = true;
+                        break;
+                    }
+                }
+
+                if (contains)
+                {
+                    uniqDictionary[i]++;
+                }
+                else
+                {
+                    uniqDictionary[i] = 1;
+                }
+            }
+            return uniqDictionary;
+        }
+    }
+}
---
+++ ListTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTest
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //a
+            Console.WriteLine($"Коллекция целых чисел");
+            List<int> integerList = new List<int>() { 
+                2, 1, 2, 3, 4, 54, -12, 3, 4, 2, 1, 4, 7, 35, -16, 13, 4, 
+            };
+            Dictionary<int, int> uniqInteger = new Dictionary<int, int>();
+
+            foreach (int i in integerList)
+            {
+                if (uniqInteger.ContainsKey(i))
+                {
+                    uniqInteger[i]++;
+                }
+                else
+                {
+                    uniqInteger[i] = 1;
+                }
+            }
+
+            foreach (KeyValuePair<int, int> p in uniqInteger)
+            {
+                Console.WriteLine($"Элемент {p.Key} встречается {p.Value} раз");
+            }
+            Console.WriteLine();
+
+            //b
+            Console.WriteLine($"Коллекция строк");
+            List<string> stringList = new List<string>() { 
+                "adf", "dgs", "asd", " adf", "0afs9", "gfd23", "adf", "asdf" 
+            };
+
+            var uniqGeneric = stringList.UniqCount();
+
+            foreach (var p in uniqGeneric)
+            {
+                Console.WriteLine($"Элемент {p.Key} встречается {p.Value} раз");
+            }
+            Console.WriteLine();
+
+            // с
+            Console.WriteLine($"Коллекция строк с использованием Linq");
+
+            var uniqLinq = stringList.GroupBy(p => p).Select(g => new { g.Key, Count = g.Count() });
+            foreach (var p in uniqLinq)
+            {
+                Console.WriteLine($"Элемент {p.Key} встречается {p.Count} раз");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
