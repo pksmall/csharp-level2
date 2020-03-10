306e097dc215b3b114ec645e7bbcdda29a8c61ca
Pavel Korzhenko <pksmall@live.ru>
2020-03-10 23:23:55 +0200

    Triggers

---
+++ Tests/BindingTest/BindingTestApp/Converters/BoolVisibility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BindingTestApp.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    class BoolVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bool_value = System.Convert.ToBoolean(value);
+            if (bool_value)
+            {
+                return Visibility.Visible;
+            } 
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visibility = System.Convert.ChangeType(value, typeof(Visibility));
+            switch (visibility)
+            {
+                case Visibility.Hidden:
+                    return false;
+                case Visibility.Visible:
+                    return true;
+                default: throw new NotSupportedException($"Value {visibility} is not supported");
+            }
+        }
+    }
+}
