24a63b42b0d99aa6fe957527d7f02065f9eb9a29
Pavel Korzhenko <pksmall@live.ru>
2020-03-11 21:31:14 +0200

    binding test and inotifypropertychanged

---
+++ Tests/BindingTest/EmployeesManager/Data/TestData.cs
@@ -0,0 +1,23 @@
+﻿using EmployeesManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManager.Data
+{
+    class TestData
+    {
+        public static List<Employee> Employees { get; } = Enumerable.Range(1, 100)
+            .Select(i => new Employee
+            {
+                Id = i,
+                Name = $"Name {i}",
+                SurName = $"Surname {i}",
+                Patronymic = $"Patronumic {i}",
+                DayOfBirth = DateTime.Now.Subtract(TimeSpan.FromDays(365/6 * (i + 18)))
+
+            }).ToList();
+    }
+}
