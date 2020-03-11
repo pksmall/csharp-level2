25190ead25610f6f3585aa01e2ac0b20f8eceff8
Pavel Korzhenko <pksmall@live.ru>
2020-03-11 22:47:33 +0200

    base view model

---
+++ Tests/MVVMTestApp/ViewModels/MainWindowNewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Timers;
+
+namespace MVVMTestApp.ViewModels
+{
+    class MainWindowNewModel
+    {
+        private Timer _Timer;
+        public DateTime CurrentTime { get; set; }
+        public string Title { get; set; } = "Title windows MVVM App";
+        public MainWindowNewModel()
+        {
+            _Timer = new Timer(100) { AutoReset = true };
+            _Timer.Elapsed += OnTimerTick;
+            _Timer.Start();            
+        }
+
+        private void OnTimerTick(object Sender, ElapsedEventArgs E)
+        {
+            CurrentTime = DateTime.Now;
+        }
+    }
+}
