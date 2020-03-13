9692c50642d6235631524e4b0c0a726a0a2d28bb
Pavel Korzhenko <pksmall@live.ru>
2020-03-14 00:33:45 +0200

    complete hw06

---
+++ Tests/WpfAppEmployee/Data/TestData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WpfAppEmployee.Models;
+
+namespace WpfAppEmployee.Data
+{
+    class TestData
+    {
+        static Random random = new Random();
+
+        public static List<Department> Departments { get; } = Enumerable.Range(1, 5)
+            .Select(i => new Department
+            {
+                DepId = i,
+                DepName = $"Department {i}"
+            }).ToList();
+
+        public static List<Employee> Employees { get; } = Enumerable.Range(1, 10)
+            .Select(i => new Employee
+            {
+                Id = i,
+                Name = $"Name {i}",
+                SurName = $"Surname {i}",
+                Patronymic = $"Patronymic {i}",
+                DayOfBirth = DateTime.Now.Subtract(TimeSpan.FromDays(365 / 6 * (i + 18))),
+                Department = Departments[random.Next(Departments.Count)].DepId
+            }).ToList();
+    }
+}
---
+++ Tests/WpfAppEmployee/Views/EditDepartament.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppEmployee
+{
+    /// <summary>
+    /// Interaction logic for EditDepartament.xaml
+    /// </summary>
+    public partial class EditDepartament : Window
+    {
+        public EditDepartament()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
